426d3e07a433643fd3c0e65d2ec601f431d718e9
ArunaSai1611 <167323113+ArunaSai1611@users.noreply.github.com>
2025-09-05 18:07:12 +0530

    Add files via upload

---
+++ src/main/java/com/excelr/FirstRealTimeSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.excelr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRealTimeSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRealTimeSpringbootProjectApplication.class, args);
+	}
+
+}
